3cfdfd15810eefeb3879d6805137023bd34f1fb9
Andrew Filkin <andrewfilkin@MacBook-Air-Andrew.local>
2024-03-09 15:07:23 +0200

    lesson 4

--- cmd/main.go
+++ cmd/main.go
@@ -6,16 +6,28 @@ import (
 	"github.com/AndrewFilkin/todo-app/pkg/repository"
 	"github.com/AndrewFilkin/todo-app/pkg/service"
 	"log"
+	"github.com/spf13/viper"
 )
 
 func main() {
+
+	if err := initConfig(); err != nil {
+		log.Fatalf("error initializing configs: %s", err.Error())
+	}
+
 	repos := repository.NewRepository()
 	services := service.NewService(repos)
 	handlers := handler.NewHandler(services)
 
 	srv := new(todo.Server)
-	err := srv.Run("8000", handlers.InitRoutes())
+	err := srv.Run(viper.GetString("8000"), handlers.InitRoutes())
 	if err != nil {
 		log.Fatalf("error occured while running http server %s", err.Error())
 	}
+}
+
+func initConfig() error {
+	viper.AddConfigPath("configs")
+	viper.SetConfigName("config")
+	return viper.ReadInConfig()
 }
